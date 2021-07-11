93f64a4cf5f4492ae18bd958755a65d850ba60cb
okamoto <okamoto_yu-ki@outlook.jp>
2021-07-11 14:30:03 +0900

    配列操作を作成した

---
+++ array/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let bytes = [0; 5];
+    // output array
+    println!("days = {:?}", days);
+    println!("bytes = {:?}", bytes);
+
+    let first = days[0];
+    let second = days[1];
+    println!("first = {}", first);
+    println!("second = {}", second);
+}
