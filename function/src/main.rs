e3862d3da0a593cc44f7027850abae0beee9c3e5
okamoto <okamoto_yu-ki@outlook.jp>
2021-07-11 09:51:53 +0900

    処理の変更

--- function/src/main.rs
+++ function/src/main.rs
@@ -4,21 +4,20 @@ fn is_divisible_by(dividend: u32, divisor: u32) -> bool {
         // To prevent division by zero, halt execution and return to the caller
         return false;
     } else if dividend % divisor > 0 {
-        println!("\n{} % {} has a remainder of {}.", dividend, divisor, ())
+        println!("\n{} % {} has a remainder of {}.", dividend, divisor, (dividend % divisor));
+    } else {
+        println!("\n{} % {} has no remainder.", dividend, divisor);
     }
+
+    // Create the boolean value and return it to the function caller
+    dividend % divisor == 0
 }
 
-
-
-fn is_zero(input: u8) -> bool {
-    if input == 0 {
-        return true;
-    }
-    false
-}
 
 fn main() {
-    if is_zero(0) {
-        println!("The value is zero.");
-    }
+    let _dividend = 15;
+    let _divisor = 3;
+    let is_divisible = is_divisible_by(_dividend, _divisor);
+    
+    println!("{}", is_divisible);
 }
