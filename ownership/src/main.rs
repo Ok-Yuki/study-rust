db3b7828e500af6a906601ba036d708aa231f5e2
okamoto <okamoto_yu-ki@outlook.jp>
2021-07-17 22:53:04 +0900

    補足を追記

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -5,7 +5,8 @@ fn process(s: String) {
 
 fn main() {
     let s = String::from("Hello, world!");
-    // process(s); とした場合、10行目でコンパイルエラー
+    // process(s); とした場合、11行目でコンパイルエラー
+    // cloneを使用して複製すれば回避できるが、オーバーヘッドがかかる。
     process(s.clone());
     process(s);
 }
