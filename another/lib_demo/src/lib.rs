362e8f32ace191a9b43965950c90c0212276f8ce
okamoto <okamoto_yu-ki@outlook.jp>
2021-12-12 16:38:35 +0900

    ライブラリを作成

--- another/lib_demo/src/lib.rs
+++ another/lib_demo/src/lib.rs
@@ -1,7 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod generator;
+pub fn print_random_number() {
+    let n = generator::gen_ran();
+    println!("Random u8: {}", n);
 }
